637dc92978600dfca912b6341b3cfe801a58fce9
Desi Mandasari <desimandasari1993@gmail.com>
2019-01-29 15:55:35 +0700

    setting up redux

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,19 @@
+import { SIGN_IN, SET_SIGN_UP_STATUS, SIGN_OUT } from '../types'
+
+const initialState = {
+    isAuthenticated: false,
+    isSignUpSuccess: false
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_SIGN_UP_STATUS:
+            return { isSignUpSuccess: action.payload }
+        case SIGN_IN:
+            return { isAuthenticated: true }
+        case SIGN_OUT:
+            return { isAuthenticated: false }
+        default:
+            return state
+    }
+}
