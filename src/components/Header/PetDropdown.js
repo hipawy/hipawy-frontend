02fbd052ec44976e9d6506ae572c522a3f14fcdd
Desi Mandasari <desimandasari1993@gmail.com>
2019-02-04 19:17:43 +0700

    add addPet modals

--- src/components/Header/PetDropdown.js
+++ src/components/Header/PetDropdown.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   Dropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  DropdownItem,
+  Modal
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { signOut } from "../../store/actions/auth";
 import { Redirect } from "react-router-dom";
+import AddPet from "../LoginRegister/AddPet";
 
 const Button = styled(DropdownToggle)`
   padding: 8px;
@@ -31,18 +33,24 @@ class UserDropDown extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false,
+      modalOpen: false,
       isRedirectedtoProfile: false
     };
   }
 
-  toggle() {
+  toggleModal = () => {
+    this.setState(prevState => ({
+      modalOpen: !prevState.modalOpen
+    }));
+  };
+
+  toggle = () => {
     this.setState(prevState => ({
       dropdownOpen: !prevState.dropdownOpen
     }));
-  }
+  };
 
   toProfile = e => {
     this.setState({
@@ -58,20 +66,23 @@ class UserDropDown extends React.Component {
     }
 
     return (
-      <DropDown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <Button>
-          <i className="fa fa-plus-square" />
-          <span style={{ marginLeft: "8px" }} />
-          <i className="fa fa-caret-down" />
-        </Button>
-        <DropdownMenu>
-          <DropdownItem tag={Link} to="/UserProfile">
-            Profile
-          </DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem onClick={signOut}>Sign Out</DropdownItem>
-        </DropdownMenu>
-      </DropDown>
+      <Fragment>
+        <DropDown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <Button>
+            <i className="fa fa-plus-square" />
+            <span style={{ marginLeft: "8px" }} />
+            <i className="fa fa-caret-down" />
+          </Button>
+          <DropdownMenu>
+            <DropdownItem onClick={this.toggleModal}>Add Pet</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>Delete Pet</DropdownItem>
+          </DropdownMenu>
+        </DropDown>
+        <Modal isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+          <AddPet />
+        </Modal>
+      </Fragment>
     );
   }
 }
