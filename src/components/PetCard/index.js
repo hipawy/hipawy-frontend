a2808fd87dae1752e06efab6aa352a071e03a38a
Desi Mandasari <desimandasari1993@gmail.com>
2019-02-05 17:10:25 +0700

    modified index.js in petCard, and delete index.js in userprofile

--- src/components/PetCard/index.js
+++ src/components/PetCard/index.js
@@ -1,10 +1,57 @@
-// import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+import { Button } from "reactstrap";
+import { connect } from "react-redux";
+import { deletePet, fetchUserPets } from "../../store/actions/pets";
+import { Redirect } from "react-router-dom";
 
-// class  extends Component {
-//   state = {  }
-//   render() {
-//     return (  );
-//   }
-// }
+class PetCard extends Component {
+  state = {
+    isComplete: false
+  };
 
-// export default ;
+  handleClick = () => {
+    if (this.props.user) {
+      this.props.deletePet({
+        userId: this.props.user.id,
+        petId: this.props.pet.id
+      });
+      this.props.fetchUserPets(this.props.user.id);
+
+      this.setState({ isComplete: true });
+    }
+  };
+
+  render() {
+    const { pet } = this.props;
+
+    if (this.state.isComplete) {
+      return <Redirect to="/UserProfile" />;
+    }
+
+    return (
+      <Fragment>
+        <div>
+          <img src={pet.photo} alt="pet image" />
+          <h3>{pet.name}</h3>
+          <h6>{pet.category}</h6>
+          <h6>{pet.breed}</h6>
+          <h6>{pet.age}</h6>
+          <h6>{pet.desc}</h6>
+        </div>
+        <div>
+          <Button>Edit</Button>
+          <Button onClick={this.handleClick}>Delete</Button>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = store => ({
+  user: store.auth.user
+});
+
+export default connect(
+  mapStateToProps,
+  { deletePet, fetchUserPets }
+)(PetCard);
