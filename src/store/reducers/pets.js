a31cfa4b3ac2aa90bb0e14444a6ffec109dacc9e
Desi Mandasari <desimandasari1993@gmail.com>
2019-02-06 22:58:28 +0700

    fix fetching data for user in modal

--- src/store/reducers/pets.js
+++ src/store/reducers/pets.js
@@ -1,14 +1,21 @@
-import { FETCH_PETS, FETCH_USER_PETS } from "../types";
+import {
+  FETCH_PETS,
+  FETCH_USER_PETS,
+  FETCH_USER_DATA
+} from "../types";
 
 const initialState = {
   pets: [],
-  userPets: []
+  userPets: [],
+  user: null
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_PETS:
       return { ...state, pets: action.payload };
+    case FETCH_USER_DATA:
+      return { ...state, user: action.payload };
     case FETCH_USER_PETS:
       return { ...state, userPets: action.payload };
     default:
