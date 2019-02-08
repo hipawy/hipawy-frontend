02f820d946585298f1cf781ad7200cc487706255
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-02-08 18:02:42 +0700

    styled filter component

--- src/components/HomeSearch/index.js
+++ src/components/HomeSearch/index.js
@@ -1,9 +1,23 @@
 import React, { Component, Fragment } from "react";
 import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
+import styled from "styled-components";
 import petsearch from "./Pettype";
 import provinces from "../Adress/data";
 import { fetchPets } from "../../store/actions/pets";
 import { connect } from "react-redux";
+
+const StyledRow = styled(Row)`
+  margin: 5vh 5vw;
+`;
+const StyledCol = styled(Col)`
+  margin: 3vh 0;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 class Search extends Component {
   state = {
@@ -22,7 +36,7 @@ class Search extends Component {
   handleSubmit = e => {
     e.preventDefault();
 
-    const { data, provinces, ...state  } = this.state;
+    const { data, provinces, ...state } = this.state;
     this.props.fetchPets(
       this.state.category,
       this.state.breed,
@@ -32,7 +46,7 @@ class Search extends Component {
   };
 
   componentDidMount() {
-    this.setState({ data: petsearch, provinces});
+    this.setState({ data: petsearch, provinces });
   }
 
   clearSearch = e => {
@@ -44,100 +58,106 @@ class Search extends Component {
     const { data, category, breed, provinces, province, city } = this.state;
     return (
       <Fragment>
-        <Form onSubmit={this.handleSubmit}>
-          <Row form>
-            <Col md={3}>
-              <FormGroup>
-                <Label for="Category">Animal</Label>
-                <Input
-                  type="select"
-                  name="category"
-                  id="SelectCategoryPet"
-                  onChange={this.handleChange}
-                  value={category}
-                >
-                  <option disabled value="">
-                    Choose One
-                  </option>
-                  {data &&
-                    Object.keys(data).map((category, i) => (
-                      <option value={category} key={i}>
-                        {category}
-                      </option>
-                    ))}
-                </Input>
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for="Breed">Breed</Label>
-                <Input
-                  type="select"
-                  name="breed"
-                  id="SelectBreed"
-                  onChange={this.handleChange}
-                  value={breed}
-                >
-                  <option disabled value="">
-                    Choose one
-                  </option>
-                  {category !== "" &&
-                    data[category].map((breed, i) => (
-                      <option value={breed} key={i}>
-                        {breed}
-                      </option>
-                    ))}
-                </Input>
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for="Province">Province</Label>
-                <Input
-                  type="select"
-                  name="province"
-                  id="SelectProvincePet"
-                  onChange={this.handleChange}
-                  value={province}
-                >
-                  <option disabled value="">
-                    Choose One
-                  </option>
-                  {provinces &&
-                    Object.keys(provinces).map((province, i) => (
-                      <option value={province} key={i}>
-                        {province}
-                      </option>
-                    ))}
-                </Input>
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <Label for="City">City</Label>
-                <Input
-                  type="select"
-                  name="city"
-                  id="SelectCity"
-                  onChange={this.handleChange}
-                  value={city}
-                >
-                  <option disabled value="">
-                    Choose one
-                  </option>
-                  {province !== "" &&
-                    provinces[province].map((city, i) => (
-                      <option value={city} key={i}>
-                        {city}
-                      </option>
-                    ))}
-                </Input>
-              </FormGroup>
-            </Col>
-          </Row>
-          <Button type="submit">Search</Button>
-          <Button onClick={this.clearSearch}>Clear</Button>
-        </Form>
+        <StyledDiv>
+          <Form onSubmit={this.handleSubmit}>
+            <StyledRow form>
+              <Col md={2}>
+                <FormGroup>
+                  <Label for="Category">Animal</Label>
+                  <Input
+                    type="select"
+                    name="category"
+                    id="SelectCategoryPet"
+                    onChange={this.handleChange}
+                    value={category}
+                  >
+                    <option disabled value="">
+                      Choose One
+                    </option>
+                    {data &&
+                      Object.keys(data).map((category, i) => (
+                        <option value={category} key={i}>
+                          {category}
+                        </option>
+                      ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup>
+                  <Label for="Breed">Breed</Label>
+                  <Input
+                    type="select"
+                    name="breed"
+                    id="SelectBreed"
+                    onChange={this.handleChange}
+                    value={breed}
+                  >
+                    <option disabled value="">
+                      Choose one
+                    </option>
+                    {category !== "" &&
+                      data[category].map((breed, i) => (
+                        <option value={breed} key={i}>
+                          {breed}
+                        </option>
+                      ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup>
+                  <Label for="Province">Province</Label>
+                  <Input
+                    type="select"
+                    name="province"
+                    id="SelectProvincePet"
+                    onChange={this.handleChange}
+                    value={province}
+                  >
+                    <option disabled value="">
+                      Choose One
+                    </option>
+                    {provinces &&
+                      Object.keys(provinces).map((province, i) => (
+                        <option value={province} key={i}>
+                          {province}
+                        </option>
+                      ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup>
+                  <Label for="City">City</Label>
+                  <Input
+                    type="select"
+                    name="city"
+                    id="SelectCity"
+                    onChange={this.handleChange}
+                    value={city}
+                  >
+                    <option disabled value="">
+                      Choose one
+                    </option>
+                    {province !== "" &&
+                      provinces[province].map((city, i) => (
+                        <option value={city} key={i}>
+                          {city}
+                        </option>
+                      ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+              <StyledCol md={1.5}>
+                <Button type="submit">Search</Button>
+              </StyledCol>
+              <StyledCol md={1.5}>
+                <Button onClick={this.clearSearch}>Clear</Button>
+              </StyledCol>
+            </StyledRow>
+          </Form>
+        </StyledDiv>
       </Fragment>
     );
   }
